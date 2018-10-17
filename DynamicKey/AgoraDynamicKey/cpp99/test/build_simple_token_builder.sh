b3ff85be8cc81e809c04813e166bd04955228853
linshibo <linshibo@gmail.com>
2018-10-17 10:52:32 +0000

    fix build.sh and make compile pass

--- DynamicKey/AgoraDynamicKey/cpp99/test/build_simple_token_builder.sh
+++ DynamicKey/AgoraDynamicKey/cpp99/test/build_simple_token_builder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -ggdb -std=c++98 -O0 -I../../../ SimpleTokenBuilder_test.cpp  main.cpp -lcrypto  -lgtest -lpthread -lz -o TestSimpleTokenBuilder.exe
+g++ -ggdb  -std=c++98 -O0 -I../../../ SimpleTokenBuilder_test.cpp  main.cpp -lcrypto  -lgtest -lpthread -lz -o TestSimpleTokenBuilder.exe
